d0106d7b4bfdeacb7c2736a389970df2acc43ba3
Aharon kosetti <221158300+aharon-kumar-kosetti@users.noreply.github.com>
2025-12-29 22:12:39 +0530

    Update multiplymatrices.c

--- set3/multiplymatrices.c
+++ set3/multiplymatrices.c
@@ -1,37 +1,38 @@
 #include <stdio.h>
+
 int main() {
-int A[10][10], B[10][10], C[10][10];
-int r1, c1, r2, c2;
-int i, j, k;
-printf("Enter rows and columns of matrix A: ");
-scanf("%d %d", &r1, &c1);
-printf("Enter rows and columns of matrix B: ");
-scanf("%d %d", &r2, &c2);
-if (c1 != r2) {
-printf("Matrix multiplication not possible\n");
-return 0;
-}
-printf("Enter elements of matrix A:\n");
-for (i = 0; i < r1; i++)
-for (j = 0; j < c1; j++)
-scanf("%d", &A[i][j]);
-printf("Enter elements of matrix B:\n");
-for (i = 0; i < r2; i++)
-for (j = 0; j < c2; j++)
-scanf("%d", &B[i][j]);
-for (i = 0; i < r1; i++) {
-for (j = 0; j < c2; j++) {
-C[i][j] = 0;
-for (k = 0; k < c1; k++) {
-C[i][j] += A[i][k] * B[k][j];
-}
-}
-}
-printf("Resultant Matrix C:\n");
-for (i = 0; i < r1; i++) {
-for (j = 0; j < c2; j++)
-printf("%d ", C[i][j]);
-printf("\n");
-}
-return 0;
+    int a[10][10], b[10][10], c[10][10];
+    int n, i, j, k;
+
+    printf("Enter order of matrix: ");
+    scanf("%d", &n);
+
+    printf("Enter first matrix:\n");
+    for(i = 0; i < n; i++)
+        for(j = 0; j < n; j++)
+            scanf("%d", &a[i][j]);
+
+    printf("Enter second matrix:\n");
+    for(i = 0; i < n; i++)
+        for(j = 0; j < n; j++)
+            scanf("%d", &b[i][j]);
+
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            c[i][j] = 0;
+            for(k = 0; k < n; k++) {
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    printf("Resultant matrix:\n");
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            printf("%d ", c[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
