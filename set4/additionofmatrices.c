d86955fa0d4c5798e83976bd8fd1b409f49441ea
Aharon kosetti <221158300+aharon-kumar-kosetti@users.noreply.github.com>
2025-12-29 22:35:45 +0530

    Create additionofmatrices.c

---
+++ set4/additionofmatrices.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+int A[10][10], B[10][10], C[10][10];
+int r, c, i, j;
+printf("Enter number of rows and columns: ");
+scanf("%d %d", &r, &c);
+printf("Enter elements of first matrix:\n");
+for (i = 0; i < r; i++)
+for (j = 0; j < c; j++)
+scanf("%d", &A[i][j]);
+printf("Enter elements of second matrix:\n");
+for (i = 0; i < r; i++)
+for (j = 0; j < c; j++)
+scanf("%d", &B[i][j]);
+for (i = 0; i < r; i++)
+for (j = 0; j < c; j++)
+C[i][j] = A[i][j] + B[i][j];
+printf("Sum of the matrices:\n");
+for (i = 0; i < r; i++) {
+for (j = 0; j < c; j++)
+printf("%d ", C[i][j]);
+printf("\n");
+}
+return 0;
+}
